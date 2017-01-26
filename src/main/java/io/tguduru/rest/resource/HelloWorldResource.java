1486888f43288b5d91d96b20bc36fc49f2b2eb66
Thirupathi Reddy Guduru <tguduru@users.noreply.github.com>
2017-01-25 22:35:47 -0600

    added HEAD method

--- src/main/java/io/tguduru/rest/resource/HelloWorldResource.java
+++ src/main/java/io/tguduru/rest/resource/HelloWorldResource.java
@@ -1,20 +1,20 @@
 package io.tguduru.rest.resource;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Optional;
+import io.tguduru.rest.model.HelloWorldResponse;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
-import io.tguduru.rest.model.HelloWorldResponse;
-
-import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A REST resource implementation for hello world
+ *
  * @author Thirupathi Reddy Guduru
  * @modified Jan 18, 2015
  */
@@ -40,6 +40,12 @@ public class HelloWorldResource {
     public HelloWorldResponse helloWorld(@QueryParam("name") final Optional<String> name) {
         final String val = String.format(template, name.or(defaultName));
         return new HelloWorldResponse(val, counter.incrementAndGet());
+    }
+
+    @HEAD
+    @Timed
+    public void getHead() {
+        System.out.println("Executing HEAD method");
     }
 
 }
